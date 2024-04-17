5d2038aaecfd917522f9ddd08121f401a2b5e143
5sarad <saradpoudel05@gmail.com>
2024-04-17 21:16:12 +1000

    installed react, vite, and tailwind

---
+++ my-blog/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl font-bold text-center' >My Awesome Blog</h1>
+        <p>Welcome to my blog!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
